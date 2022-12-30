1eeb427fb461d6645d1dcf8388ca181a79b6c266
Vladimir <kirill_kudryavtsevlive@mail.ru>
2022-12-30 20:04:16 +0300

    minor changes

---
+++ mob_nav.js
@@ -0,0 +1,8 @@
+function open_nav() {
+  document.getElementById("mob_sidepanel").style.width = "150px";
+  document.getElementById("op_but").style.opacity = "0%";
+}
+function close_nav() {
+  document.getElementById("mob_sidepanel").style.width = "0";
+  document.getElementById("op_but").style.opacity = "100%";
+}
